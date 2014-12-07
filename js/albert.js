ca0d8751e6d473603e4d873cb9317f53d628160c
stodoriko <s.todorikos@gmail.com>
2014-12-07 15:37:31 +0300

    albert

---
+++ js/albert.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+   function touchAlbert() {
+    var userName = prompt("Меня зовут Альберт, а как вас зовут?", "Введите ваше имя.");
+
+    if (userName) {
+      alert("Добро пожаловать на наш сайт, " + userName +".");
+    }
+   }
+ </script>
