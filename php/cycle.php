d1738ae3f8f49b5947b37a8096049b518952d607
stodoriko <STodoriko@ARSTEL-6670.bee.vimpelcom.ru>
2015-03-09 21:51:14 +0300

    ADD gif counter

--- php/cycle.php
+++ php/cycle.php
@@ -1,9 +1,9 @@
 <?
-/*	$start = $_GET['a']; $end = $_GET['b'];
-	while ($start <= $end) {
-		echo $start, "";
-		$start++;
-	}*/
+/*  $start = $_GET['a']; $end = $_GET['b'];
+  while ($start <= $end) {
+    echo $start, "";
+    $start++;
+  }*/
 ?>
 
 <?
@@ -11,28 +11,51 @@
 ?>
 
 <?
-/*	$start = $_GET['a']; $end = $_GET['b'];
-	for ($value = $start; $value <= $end ; $value++) { 
-		echo $value. "";
-	}*/
+/*  $start = $_GET['a']; $end = $_GET['b'];
+  for ($value = $start; $value <= $end ; $value++) { 
+    echo $value. "";
+  }*/
 ?>
 
 <?
-/*	for ($r = 1; $r <= 10; $r++) { 
-		for ($c = 1; $c <= 10 ; $c++) echo ($r * $c);
-			echo "<br>";
-	}*/
+/*  for ($r = 1; $r <= 10; $r++) { 
+    for ($c = 1; $c <= 10 ; $c++) echo ($r * $c);
+      echo "<br>";
+  }*/
 ?>
 
-	<table cellpadding ="5" border="1">
-	<?
-		for ($r = 1; $r <= 10 ; $r++) { 
-				echo "<tr>";
-				for ($c = 1; $c <= 10 ; $c++) { 
-					echo "<td>";
-					echo ($r * $c);
-					echo "</td>";
-				}
-				echo "</tr>";
-			}	
-	?>
+<!--   <table cellpadding ="5" border="1">
+  <?
+    for ($r = 1; $r <= 10 ; $r++) { 
+        echo "<tr>";
+        for ($c = 1; $c <= 10 ; $c++) { 
+          echo "<td>";
+          echo ($r * $c);
+          echo "</td>";
+        }
+        echo "</tr>";
+      } 
+  ?> -->
+
+<?
+  $file_name = "counter.txt";
+  if (!file_exists($file_name)) {
+    $fp = fopen($file_name, "w");
+    fwrite($fp, "1");
+    fclose($fp);
+  }
+  $fp = fopen($file_name, "r");
+  $counter = fread($fp, 10);
+  fclose($fp);
+  $counter++;
+  $fp = fopen($file_name, "w");
+  fwrite($fp, $counter);
+  fclose($fp);
+
+  $number = strlen($counter);
+  $counter_graphic = " ";
+  for ($pos=0; $pos < $number; $pos++) { 
+    $digit = substr($counter, $pos, 1);
+    $counter_graphic .= "<img src=\"$digit.gif\">";
+  }
+?>
--- php/date.php
+++ php/date.php
@@ -1,33 +1,33 @@
 <? 
-	$day_of_the_week = date("l");
-	switch ($day_of_the_week) {
-		case 'Monday': $day_of_the_week = 'Понедельник'; break;
-		case 'Tuesday': $day_of_the_week = 'Вторник'; break;
-		case 'Wednesday': $day_of_the_week = 'Среда'; break;
-		case 'Thursday': $day_of_the_week = 'Четверг'; break;
-		case 'Friday': $day_of_the_week = 'Пятница'; break;
-		case 'Saturday': $day_of_the_week = 'Суббота'; break;
-		case 'Sunday': $day_of_the_week = 'Воскресенье'; break;
-	}
+  $day_of_the_week = date("l");
+  switch ($day_of_the_week) {
+    case 'Monday': $day_of_the_week = 'Понедельник'; break;
+    case 'Tuesday': $day_of_the_week = 'Вторник'; break;
+    case 'Wednesday': $day_of_the_week = 'Среда'; break;
+    case 'Thursday': $day_of_the_week = 'Четверг'; break;
+    case 'Friday': $day_of_the_week = 'Пятница'; break;
+    case 'Saturday': $day_of_the_week = 'Суббота'; break;
+    case 'Sunday': $day_of_the_week = 'Воскресенье'; break;
+  }
 
-	$month = date("F");
-	switch ($month) {
-		case 'January': $month = 'Января'; break;
-		case 'February': $month = 'Февраля'; break;
-		case 'March': $month = 'Марта'; break;
-		case 'April': $month = 'Апреля'; break;
-		case 'May': $month = 'Мая'; break;
-		case 'June': $month = 'Июня'; break;
-		case 'July': $month = 'Июля'; break;
-		case 'August': $month = 'Августа'; break;
-		case 'September': $month = 'Сентября'; break;
-		case 'October': $month = 'Октября'; break;
-		case 'November': $month = 'Ноября'; break;
-		case 'December': $month = 'Декабря'; break;		
-	}
+  $month = date("F");
+  switch ($month) {
+    case 'January': $month = 'Января'; break;
+    case 'February': $month = 'Февраля'; break;
+    case 'March': $month = 'Марта'; break;
+    case 'April': $month = 'Апреля'; break;
+    case 'May': $month = 'Мая'; break;
+    case 'June': $month = 'Июня'; break;
+    case 'July': $month = 'Июля'; break;
+    case 'August': $month = 'Августа'; break;
+    case 'September': $month = 'Сентября'; break;
+    case 'October': $month = 'Октября'; break;
+    case 'November': $month = 'Ноября'; break;
+    case 'December': $month = 'Декабря'; break;   
+  }
 
-	$date = date("d").". $month ".date("Y");
-	$time = date("H:i");
-	echo "Сегодня $day_of_the_week, $date. <br>";
-	echo "Текущее время $time. <br>";
+  $date = date("d").". $month ".date("Y");
+  $time = date("H:i");
+  echo "Сегодня $day_of_the_week, $date. <br>";
+  echo "Текущее время $time. <br>";
 ?>
