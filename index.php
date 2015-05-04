a10a8bc30e3fa63f58fa817bdda73bee01ab3484
stodoriko <s.todorikos@gmail.com>
2015-05-04 23:05:23 +0300

    ADD yandex metrix

--- index.php
+++ index.php
@@ -7,7 +7,40 @@
     <link href="index.css" type="text/css" rel="stylesheet">
   </head>
 
-  <body>
+  <body <!— Yandex.Metrika informer —>
+<a href="https://metrika.yandex.ru/stat/?id=28372591&amp;f.."
+target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/28372591/2_0_58C5BCFF_38A59CFF_.."
+style="width:80px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры)" /></a>
+<!— /Yandex.Metrika informer —>
+
+<!— Yandex.Metrika counter —>
+<script type="text/javascript">
+(function (d, w, c) {
+(w[c] = w[c] || []).push(function() {
+try {
+w.yaCounter28372591 = new Ya.Metrika({id:28372591,
+webvisor:true,
+clickmap:true,
+trackLinks:true,
+accurateTrackBounce:true});
+} catch(e) { }
+});
+
+var n = d.getElementsByTagName("script")[0],
+s = d.createElement("script"),
+f = function () { n.parentNode.insertBefore(s, n); };
+s.type = "text/javascript";
+s.async = true;
+s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+if (w.opera == "[object Opera]") {
+d.addEventListener("DOMContentLoaded", f, false);
+} else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/28372591" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!— /Yandex.Metrika counter —>>
+
   <?php
       $file_name = "counter.txt";
       // существует ли файл в котором хранится значение счётчика
